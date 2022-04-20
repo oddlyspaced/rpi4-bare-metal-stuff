b9db288740f6fcde90a76f7349ee6e0bc9516490
oddlyspaced <oddlyspaced1@gmail.com>
2022-04-20 11:58:30 +0530

    test mailbox in kernel console

--- src/kernel.c
+++ src/kernel.c
@@ -24,7 +24,7 @@ void kernel_main() {
     irq_init_vectors();
     enable_interrupt_controller();
     irq_enable();
-    // timer_init();
+    timer_init();
     
     printf("\nMAILBOX:\n");
     printf("hello?\n");
@@ -34,13 +34,14 @@ void kernel_main() {
     printf("ARM CLOCK: %d\n", mailbox_clock_rate(CT_ARM));
 
     while (1) {
-        printf("\nMAILBOX:\n");
-        printf("hello?\n");
-        printf("CORE CLOCK: %d\n", mailbox_clock_rate(CT_CORE));
-        printf("EMMC CLOCK: %d\n", mailbox_clock_rate(CT_EMMC));
-        printf("UART CLOCK: %d\n", mailbox_clock_rate(CT_UART));
-        printf("ARM CLOCK: %d\n", mailbox_clock_rate(CT_ARM));
+        uart_send(uart_recv());
+        u32 max_temp = 0;
 
-       uart_send(uart_recv());
+        mailbox_generic_command(RPI_FIRMWARE_GET_MAX_TEMPERATURE, 0, &max_temp);
+
+        u32 cur_temp = 0;
+        mailbox_generic_command(RPI_FIRMWARE_GET_TEMPERATURE, 0, &cur_temp);
+        printf("Cur temp: %dC MAX: %dC\n", cur_temp / 1000, max_temp / 1000);
+        timer_sleep(1000);
     }
 }
